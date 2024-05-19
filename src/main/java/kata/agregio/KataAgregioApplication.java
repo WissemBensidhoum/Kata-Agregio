9dd8c05dd71f720ae1043bc5c380b60ebaa80126
Bensidhoum Wissem <wbensidhoum@takima.fr>
2024-05-19 15:48:03 +0200

    [KA] init project

---
+++ src/main/java/kata/agregio/KataAgregioApplication.java
@@ -0,0 +1,13 @@
+package kata.agregio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataAgregioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataAgregioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kata/agregio/KataAgregioApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.agregio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataAgregioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
