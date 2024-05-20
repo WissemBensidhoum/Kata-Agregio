299716366d644faf729123e2ebb10cfad2e509e2
Bensidhoum Wissem <wbensidhoum@takima.fr>
2024-05-20 15:00:06 +0200

    [KA] Update README with project details

--- src/main/java/kata/agregio/config/SpringDocConfig.java
+++ src/main/java/kata/agregio/config/SpringDocConfig.java
@@ -11,7 +11,9 @@ public class SpringDocConfig {
     public OpenAPI api() {
         Info info = new Info().title("Agregio Kata API")
                 .description("""
-                        An API for managing energy sales by Agregio across various markets. It allows creating offers with different energy quantities and prices for primary, secondary, and rapid reserves. Additionally, it facilitates listing offers per market and retrieving the parks selling energy on a specific market
+                        An API for managing energy sales by Agregio across various markets. 
+                        It allows creating offers with different energy quantities and prices for primary, secondary, and rapid reserves. 
+                        Additionally, it facilitates listing offers per market and retrieving the parks selling energy on a specific market
                         ## Owners :
                         - Wissem BENSIDHOUM wbensidhoum@takima.fr
                         """);
