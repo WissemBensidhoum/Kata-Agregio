72e2c8e65527c0901c5f0556492686932400ea19
Bensidhoum Wissem <wbensidhoum@takima.fr>
2024-05-20 14:35:17 +0200

    [KA] Testing

---
+++ src/test/java/kata/agregio/TemplateServiceTest.java
@@ -0,0 +1,56 @@
+package kata.agregio;
+
+import kata.agregio.core.PageableMapper;
+import kata.agregio.park.mapper.ParkMapper;
+import kata.agregio.park.model.Park;
+import kata.agregio.park.model.dto.ParkRequestDto;
+import kata.agregio.park.repository.ParkDao;
+import kata.agregio.park.service.impl.ParkServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static kata.agregio.core.EnergyType.Wind;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ParkServiceTest {
+    @Mock
+    private ParkDao parkDao;
+
+    @Mock
+    private ParkMapper parkMapper;
+
+    @Mock
+    private PageableMapper pageableMapper;
+
+    @InjectMocks
+    private ParkServiceImpl parkService;
+
+    @AfterEach
+    void tearDown() {
+        clearInvocations(parkDao);
+    }
+
+    @Test
+    @DisplayName("it should create park")
+    void createParkTest() {
+        // GIVEN
+        Park park = new Park.Builder().energyType(Wind)
+                .capacity(50000)
+                .build();
+
+        ParkRequestDto parkRequestDto = new ParkRequestDto(Wind, 50000);
+
+        // WHEN
+        parkService.create(parkRequestDto);
+
+        // THEN
+        verify(parkDao, times(1)).save(park);
+    }
+
+}
